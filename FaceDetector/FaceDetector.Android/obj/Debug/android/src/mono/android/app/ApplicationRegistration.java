f4491633afa649095e269ecebb774cdce945110a
Li Jong <ljon567@aucklanduni.ac.nz>
2017-07-23 19:39:08 +1200

    Integrated Camera

--- FaceDetector/FaceDetector.Android/obj/Debug/android/src/mono/android/app/ApplicationRegistration.java
+++ FaceDetector/FaceDetector.Android/obj/Debug/android/src/mono/android/app/ApplicationRegistration.java
@@ -5,6 +5,7 @@ public class ApplicationRegistration {
 	public static void registerApplications ()
 	{
 				// Application and Instrumentation ACWs must be registered first.
+		mono.android.Runtime.register ("FaceDetector.Droid.MainApplication, FaceDetector.Android, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", md5473dcf9dbec3902b330e8adb6c1bd17e.MainApplication.class, md5473dcf9dbec3902b330e8adb6c1bd17e.MainApplication.__md_methods);
 		
 	}
 }
